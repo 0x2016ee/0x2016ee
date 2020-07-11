ea86512fe1ab1c31b2d44ebacaf8cc6f1514fe89
0x2016ee <68165180+0x2016ee@users.noreply.github.com>
2020-07-12 03:50:46 +0700

    Update main3.js

--- main3.js
+++ main3.js
@@ -1,3 +1,31 @@
+function otherScript() {
+  //-----START -----//
+
+  //-----STOP -----//
+}
+
+content = document.getElementById('comments').getElementsByTagName('p');
+for (var i = 0; i < content.length; i++) {
+ if (content[i].innerHTML.indexOf('</a>') !== -1) {
+ content[i].innerHTML = content[i].innerHTML.replace(/<a[^>]*>|<\/a>/g, "");
+ }
+}
+
+function showComm(e){document.getElementById(e)&&("none"!=document.getElementById(e+"-show").style.display?(document.getElementById(e+"-show").style.display="none",document.getElementById(e).style.display="block"):(document.getElementById(e+"-show").style.display="block",document.getElementById(e).style.display="none"))};
+         
+$(document).ready(function(){$(".post-body a").on("click",function(o){if(""!==this.hash){o.preventDefault();var t=this.hash;$("html, body").animate({scrollTop:$(t).offset().top},600,function(){window.location.hash=t})}})});
+
+(function ignielRemLinkNameCmt(){
+ if (document.querySelector('#comments .comments-content')){
+  var a = document.querySelectorAll('.comment-block .user a');
+  for (var x = 0; x < a.length; x++){
+   var b = a[x].text, c = a[x].parentNode, d = document.createElement('span'); d.innerHTML = b;
+   a[x].remove();
+   c.appendChild(d);
+  }
+ }
+})();
+
 $(document).ready(function(){$("body").toggleClass(localStorage.toggled),$("#modedark").on("click",function(){"Night"!=localStorage.toggled?($("body").toggleClass("Night",!0),localStorage.toggled="Night",$(".section-center").css("display","block")):($("body").toggleClass("Night",!1),localStorage.toggled="",$(".section-center").css("display",""))}),$("body").hasClass("Night")?$("#modedark").prop("checked",!0):$("#modedark").prop("checked",!1)});
 
 $(document).ready(function () {
