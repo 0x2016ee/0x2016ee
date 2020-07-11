06d0fc289d067b6da146cb81a89e124ab4448509
0x2016ee <68165180+0x2016ee@users.noreply.github.com>
2020-07-12 03:56:15 +0700

    Update main3.js

--- main3.js
+++ main3.js
@@ -1,29 +1,3 @@
-function otherScript() {
-  //-----START -----//
-
-  //-----STOP -----//
-}
-
-$(document).ready(function () {
-    $(".close-sf").click(function () {
-        $(".Popup_Menu,.Popup_Menu3,#Tukang_Redesign_Header_Container,#wrapper").removeClass("aktif")
-    })
-}), $(document).ready(function () {
-    $(".Tombol_Popup").click(function () {
-        $(".Popup_Menu,.Popup_Menu3,#Tukang_Redesign_Header_Container,#wrapper").toggleClass("aktif")
-    })
-})
-
-$(document).ready(function () {
-    $(".close-sf").click(function () {
-        $(".Tukang_Redesign_Popup,.Tukang_Redesign_Popup3,#Tukang_Redesign_Header_Container,#wrapper").removeClass("aktif")
-    })
-}), $(document).ready(function () {
-    $(".tmblbtn").click(function () {
-        $(".Tukang_Redesign_Popup,.Tukang_Redesign_Popup3,#Tukang_Redesign_Header_Container,#wrapper").toggleClass("aktif")
-    })
-})
-
 $(document).ready(function () {
     $(".menu-toggle").click(function () {
         $("#nav-wrapper, .darkshadow").toggleClass("open")
